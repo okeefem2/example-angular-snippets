2569e11e906439781ed24de4977f1c274922b242
Michael O'Keefe <okeefem355@gmail.com>
2018-03-24 11:26:02 -0400

    Finished up submit button snippet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-
+import { SubmitButtonComponent } from './submit-button/submit-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubmitButtonComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/submit-button/submit-button.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
+@Component({
+  selector: 'submit-button',
+  templateUrl: './submit-button.component.html',
+  styleUrls: ['./submit-button.component.css'],
+  animations: [
+    trigger('buttonTextStateTrigger', [
+      state('shown', style({
+        opacity: 1
+      })),
+      state('transitioning', style({
+        opacity: 0.3
+      })),
+      transition('shown => transitioning', animate('600ms ease-out')),
+      transition('transitioning => shown', animate('600ms ease-in'))
+    ])
+  ]
+})
+export class SubmitButtonComponent {
+
+  // The current status of the button text
+  buttonTextState = 'shown';
+  // The text currently being show
+  buttonText = "ADD TO CART";
+  // The text that will be shown when the transition is finished
+  transitionButtonText = "ADD TO CART";
+
+  /**
+   * Respond to the transition event of the button text
+   * by setting the text awaiting transition then setting the state as shown
+   */
+  buttonTextTransitioned(event) {
+    this.buttonText = this.transitionButtonText;
+    this.buttonTextState = this.buttonTextState = 'shown';
+  }
+
+  onAddToCart() {
+    // Kick off the first transition
+    this.buttonTextState = 'transitioning';
+    this.transitionButtonText = 'ADDING...';
+
+    // Do whatever logic here. If it is asynchronous, put the remaining transitions in your subscribe/then callbacks
+    // Note if your logic is snappy, you could leave the timeouts in to simulate the animation for a better UX
+
+    setTimeout(() => {
+      this.buttonTextState = 'transitioning';
+      this.transitionButtonText = 'ADDED';
+    }, 1800);
+
+    // Reset button text
+    setTimeout(() => {
+      this.buttonTextState = 'transitioning';
+      this.transitionButtonText = 'ADD TO CART';
+    }, 3600);
+  }
+}
