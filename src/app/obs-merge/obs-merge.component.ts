26aa68a9931c599db9b0799e0a28a81fa18f9761
Michael O'Keefe <m.okeefe@kunzleigh.com>
2018-04-18 10:10:31 -0600

    Update button, and get rxjs example running

--- src/app/obs-merge/obs-merge.component.ts
+++ src/app/obs-merge/obs-merge.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs/observable/of';
+import { Observable } from 'rxjs/Observable';
+import { mergeMap } from 'rxjs/operators';
+import { forkJoin } from 'rxjs/observable/forkJoin';
 
 @Component({
   selector: 'app-obs-merge',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObsMergeComponent implements OnInit {
 
-  constructor() { }
+  mergedSource$: Observable<any[]>;
 
   ngOnInit() {
-  }
+    // Create first observable
+    const source1$ = of([
+      { message: 'one' }
+    ]);
 
+    // Create second observable
+    const source2$ = of([
+      { message: 'two' }
+    ]);
+
+    // Create our merge function where we edit some of the data
+    const mergeFun = (m: any[]) => {
+      m.map((d) => d.message = d.message + ' merged!');
+      return m;
+    };
+
+    // Fork join the two sources with the merge function applied to them.
+    this.mergedSource$ = forkJoin(
+      source1$.pipe(mergeMap(mergeFun)),
+      source2$.pipe(mergeMap(mergeFun))
+    );
+  }
 }
